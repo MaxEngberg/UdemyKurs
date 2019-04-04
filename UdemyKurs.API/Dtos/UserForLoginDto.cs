b113206b1707c2d57af84ae63a857615cf4a2efb
Max Engberg <engberg-max@hotmail.com>
2019-04-04 16:01:53 +0200

    End of Section 3

---
+++ UdemyKurs.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace UdemyKurs.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- UdemyKurs.API/Models/Value.cs
+++ UdemyKurs.API/Models/Value.cs
@@ -4,5 +4,7 @@ namespace UdemyKurs.API.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Test { get; set; }
     }
 }
